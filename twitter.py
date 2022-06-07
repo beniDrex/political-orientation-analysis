04556a8c385929331c7e73b2838a43793cafe559
Beni Singh <ta.singh@f5.com>
2022-06-06 20:59:39 -0700

    cleanup

--- twitter.py
+++ twitter.py
@@ -1,7 +1,6 @@
 import json
 import tweepy
 import sys
-from pprint import pprint
 import csv
 
 class User:
@@ -11,11 +10,11 @@ class User:
         self.name=name
         self.political_party=political_party
         self.tweets=[]
-        self.engaged_users=[]
-        self.shared_urls=[]
+        self.following=[]
+        # self.shared_urls=[]
     
     def __str__(self):
-        return f"Username: {self.username}, Name: {self.name}, Political Party: {self.political_party}\n Tweets: {self.tweets}, Engaged Users: {self.engaged_users}"
+        return f"Username: {self.username}, Name: {self.name}, Political Party: {self.political_party}\n Tweets: {self.tweets}"
 
 class Twitter:
     def __init__(self, auth_location="./auth.json"):
@@ -23,10 +22,6 @@ class Twitter:
             with open(auth_location, "r") as auth_file:
                 data = json.load(auth_file)
                 self.bearer_token=data["bearer_token"]
-                self.access_token=data["access_token"]
-                self.access_token_secret=data["access_token_secret"]
-                self.consumer_key=data["consumer_key"]
-                self.consumer_secret=data["consumer_secret"]
 
         except FileExistsError or FileNotFoundError or KeyError as err:
             sys.exit(f"Error: {err}. Failed to init - please check README.md")
@@ -63,39 +58,33 @@ class Twitter:
 
         return users
 
-    def scrapper(self, twitter_users):
+    def scrapper(self, twitter_users, max_depth=0):
         for i, user in enumerate(twitter_users):
             user_temp_tweets=[]
-            engaged_users=[]
-            engaged_users_object=[]
+            following_list=[]
+            # engaged_users_object=[]
             users_tweets = self.client.get_users_tweets(user.id, exclude='replies', max_results=10)
             if users_tweets and users_tweets.data:
-                for index, tweet in enumerate(users_tweets.data):
+                for tweet in users_tweets.data:
                     user_temp_tweets.append(tweet.text)
-                    # if index < 2:
-                    liking_users = self.client.get_liking_users(tweet.id)
-                    if liking_users is not None and liking_users.data is not None:
-                        for liking_user in liking_users.data:
-                            if liking_user.username not in engaged_users:
-                                # user_obj=User(liking_user.username, liking_user.id, liking_user.name, user.political_party)
-                                engaged_users.append(liking_user.username)
-                                # engaged_users_object.append(user_obj)
-
-                user.tweets=user_temp_tweets
-                user.engaged_users=engaged_users
-
-                populate_user_data(user) # save data to csv
-                # if i == 4:
-                #     break
-                if engaged_users_object:
-                    twitter_users.extend(self.scrapper(engaged_users_object))
-        return twitter_users
             
+            if max_depth > 0:
+                users_following_resp = self.client.get_users_following(user.id, max_results=5)
+                if users_following_resp and users_following_resp.data:
+                    for following in users_following_resp.data:
+                        user_obj=User(following.username, following.id, following.name, user.political_party)
+                        following_list.append(user_obj)
+
+            user.tweets=user_temp_tweets
+            populate_user_data(user) # save row to csv
+            if following_list:
+                self.scrapper(following_list, max_depth-1)
+
             
 def populate_user_data(user):
     with open('./data.csv', 'a', newline='\n') as outfile:
         csv_writer = csv.writer(outfile)
-        row = [ user.username, user.name, user.political_party, user.id, str(user.tweets), str(user.engaged_users) ]
+        row = [ user.username, user.name, user.political_party, user.id, str(user.tweets) ]
         csv_writer.writerow(row)
         
 
@@ -115,21 +104,19 @@ def starting_twitter_handles(file="./representative-twitters.csv"):
                 republicans.append(username[len(username)-1])
     return democrats, republicans
 
-
-
 def main():
     # init twitter client
     twitter=Twitter()
-    # init starting twitter handles (house representatives)
+    # starting twitter handles of house representatives (https://pressgallery.house.gov/member-data/members-official-twitter-handles)
     democrats, republicans = starting_twitter_handles()
     twitter_users = twitter.get_users("D", democrats)
     twitter_users.extend(twitter.get_users("R", republicans))
-
-    populated_twitter_users = twitter.scrapper(twitter_users)
+    # multiple users can be scrapped per account by increasing depth 
+    # Currently set to 0 since basic API key hits rate limit almost instantly if fetching followers
+    # for every account that we are looping over
+    populated_twitter_users = twitter.scrapper(twitter_users, max_depth=1)
     for user in populated_twitter_users:
         if user.tweets:
             print(user)
-    # for user in starting_users:
-    #     print(user)
 
 main()
